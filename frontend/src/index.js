f3d3f7fd3e67a902ce1edda2a726822429162cc5
Sushant Guha <sush.guha@gmail.com>
2022-05-28 21:31:38 -0400

    removing bootstrap from entrire application

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css"; // bootstrap stylesheet
-import "bootstrap/dist/css/bootstrap.min.js";
+import "bootstrap/dist/css/bootstrap.min.js"; // Check if this needed.
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
