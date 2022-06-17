7907c1ef69dcbc80027cc26f724d95975720ae9c
tmurali <tarunmurali02@gmail.com>
2022-06-16 23:22:49 -0500

    barebones userauth(will need jwt)

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,60 @@
-const express = require("express");
+ const express = require("express");
 const cors = require("cors");
 const SpotifyWebApi = require("spotify-web-api-node");
 const bodyParser = require("body-parser");
-
+const bcrypt = require("bcrypt");
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+const users = [1,2,3];
 // app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/allhosts',(req,res)=>{
+  res.json(users)
+});
+app.post('/addhost',async (req,res)=>
+{
+  try
+  {
 
+    const salt = await bcrypt.genSalt();
+
+    const hashed= await bcrypt.hash(req.body.password,salt);
+    const user = {name:req.body.name, password : hashed}
+    users.push(user);
+    console.log(users)
+    res.status(200).send();
+  }
+  catch
+  {
+    
+    res.status(400).send();
+  }
+  
+});
+app.post('/allhosts/login',async(req,res)=>
+{
+  
+  const user = users.find ((user ) => (user.name==req.body.name));
+  if (!user)
+  {
+    return res.status(400).send("This Party does not exist");
+
+  }
+  try{
+    if (await bcrypt.compare(req.body.password,user.password))  
+    {
+      res.send("Success Joining Party as Host");
+    } 
+    else
+    {
+      res.send("Invalid party host credentials");
+    }
+  }
+  catch
+  {
+    res.status(400).send();
+  }
+});
 app.post("/refresh", (req, res) => {
   const refreshToken = req.body.refreshToken;
   const spotifyApi = new SpotifyWebApi({
